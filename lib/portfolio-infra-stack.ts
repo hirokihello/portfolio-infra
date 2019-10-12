6c7c63070daf0f3b58d666ac530016763625ca8a
hirokihello <iammyeye1@gmail.com>
2019-10-12 23:05:02 +0900

    cdkのデプロイ部分を実装

--- lib/portfolio-infra-stack.ts
+++ lib/portfolio-infra-stack.ts
@@ -12,3 +12,13 @@ export class PortfolioInfraStack extends cdk.Stack {
     });
   }
 }
+
+const app = new cdk.App();
+
+new PortfolioInfraStack(app, 'MyStaticSite', { env: {
+    // Stack must be in us-east-1, because the ACM certificate for a
+    // global CloudFront distribution must be requested in us-east-1.
+    region: 'us-east-1'
+}});
+
+app.synth();
