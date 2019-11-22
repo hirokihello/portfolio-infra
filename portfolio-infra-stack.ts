a915ee71a873792df7492cc815270c140f2530ce
hirokihello <iammyeye1@gmail.com>
2019-11-22 23:02:47 +0900

    不必要なimportを削除

--- portfolio-infra-stack.ts
+++ portfolio-infra-stack.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import cdk = require('@aws-cdk/core');
 import { StaticSite } from './sites';
-import * as process from 'process';
 
 export class PortfolioInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
