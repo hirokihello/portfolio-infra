598f0f2b29f9d18d0f40bc01ad8cf6755b38c3c2
hirokihello <iammyeye1@gmail.com>
2019-11-22 21:48:37 +0900

    fix

--- lib/portfolio-infra-stack.ts
+++ lib/portfolio-infra-stack.ts
@@ -19,7 +19,7 @@ const app = new cdk.App();
 new PortfolioInfraStack(app, 'MyStaticSite', {
   env: {
     region: 'us-east-1',
-    account: process.env.AWS_ACCOUNT_ID
+    account: process.env.CDK_DEFAULT_ID || process.env.AWS_ACCOUNT_ID
 }});
 
 app.synth();
